3eab6d77066c45f9aa53d2146ebaa2dea7b72671
Derrick Cudd <instantramen125@gmail.com>
2017-02-23 06:36:14 +0000

    creates a forms partials and adds create, destroy, and edit functions to the articles controller.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,42 @@
 class ArticlesController < ApplicationController
     def index
-        @articles = Article.all
+      @articles = Article.all
     end
     
     def show
-        @article = Article.find(params[:id])
-
+      @article = Article.find(params[:id])
     end
     
+    
     def new
-        
-        
+      @article = Article.new
+    end
+    
+    # def create
+    #   @article = Article.new
+    #   @article.title = params[:article][:title]
+    #   @article.body = params[:article][:body]
+    #   @article.author = params[:article][:author]
+    #   @article.save
+    #   redirect_to article_path(@article)
+    # end
+    
+    def create
+      @article = Article.new(
+        title: params[:article][:title],
+        body:  params[:article][:body],
+        author: params[:article][:author])
+      @article.save
+      redirect_to article_path(@article)
+    end
+    
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      redirect_to articles_path
+    end
+    
+    def edit
+      @article = Article.find(params[:id])
     end
 end
