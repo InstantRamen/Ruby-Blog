73098baafab1c36cbdeefd924c2b3f855c0bde6f
Derrick Cudd <instantramen125@gmail.com>
2017-02-24 03:51:50 +0000

    finishes blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,8 @@ class ArticlesController < ApplicationController
     
     def show
       @article = Article.find(params[:id])
+      @comment = Comment.new
+      @comment.article_id = @article.id
     end
     
     
@@ -26,7 +28,7 @@ class ArticlesController < ApplicationController
       @article = Article.new(article_params)
       @article.save
       
-      flash.notice = "Article '#{@article.title}' has been created!"
+      flash.notice = "Article '#{@article.title}' has been create!"
       
       redirect_to article_path(@article)
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170205011624) do
+ActiveRecord::Schema.define(version: 20170223073517) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -20,5 +20,15 @@ ActiveRecord::Schema.define(version: 20170205011624) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author_name"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
