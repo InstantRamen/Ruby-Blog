b840311869554b65a83b56cce57dcd8383dc7836
Derrick Cudd <instantramen125@gmail.com>
2017-02-25 02:51:51 +0000

    fixes the 'Author Name' bug on the comments form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,9 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.article_id = params[:article_id]
-    
+    if @comment.author_name.strip.length <= 0 then
+      @comment.author_name = "Anon"
+    end
     @comment.save
     
     redirect_to article_path(@comment.article)
