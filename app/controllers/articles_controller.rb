fbde87faa1e3aaf4a654d9f79fe927e48e675417
Derrick Cudd <instantramen125@gmail.com>
2017-02-27 02:01:46 -0500

    Completes tagging feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,15 +15,6 @@ class ArticlesController < ApplicationController
       @article = Article.new
     end
     
-    # def create
-    #   @article = Article.new
-    #   @article.title = params[:article][:title]
-    #   @article.body = params[:article][:body]
-    #   @article.author = params[:article][:author]
-    #   @article.save
-    #   redirect_to article_path(@article)
-    # end
-    
     def create
       @article = Article.new(article_params)
       @article.save
